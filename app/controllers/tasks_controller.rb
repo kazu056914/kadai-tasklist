aad848a3fa096a20ad994c9f086396290d1d1d9b
kazuma.narita <kazu056914@gmail.com>
2019-08-08 04:15:30 +0000

    fix task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,18 +3,18 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
 
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(id: :desc)
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
@@ -48,7 +48,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
   end
 
   def task_params
